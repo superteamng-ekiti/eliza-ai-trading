09c66f96388bfaf514b0e4ede4462083b7faf2bf
inspi-writer001 <conservedinnovation@gmail.com>
2024-12-22 09:04:14 -0800

    added crossmint and configured node packages

--- plugin-crossmint/src/index.ts
+++ plugin-crossmint/src/index.ts
@@ -2,38 +2,49 @@ import type { Plugin } from "@ai16z/eliza";
 import { getOnChainActions } from "./actions";
 import { getWalletClientAndConnection, getWalletProvider } from "./wallet";
 import { splToken } from "@goat-sdk/plugin-spl-token";
-
+import { sendSOL } from "@goat-sdk/core";
+import { jupiter } from "@goat-sdk/plugin-jupiter";
+import { crossmint } from "@goat-sdk/crossmint";
 async function createCrossmintPlugin(
-    getSetting: (key: string) => string | undefined
+  getSetting: (key: string) => string | undefined
 ): Promise<Plugin> {
-    const { walletClient, connection } = await getWalletClientAndConnection(getSetting);
-    if (!walletClient) {
-        throw new Error("Wallet client not found");
-    }
-    console.log("walletClient", walletClient);
-    const actions = await getOnChainActions({
-        wallet: walletClient,
-        // Add plugins here based on what actions you want to use
-        // See all available plugins at https://ohmygoat.dev/chains-wallets-plugins#plugins
-Hacker            // Add you solana plugins here
-            splToken({
-                connection,
-                network: "mainnet",
-            }),
-            // coingecko({
-            //  apiKey: getSetting("COINGECKO_API_KEY")
-            // })
-        ],
-    });
+  const { walletClient, connection } = await getWalletClientAndConnection(
+    getSetting
+  );
+  if (!walletClient) {
+    throw new Error("Wallet client not found");
+  }
 
-    return {
-        name: "[Crossmint] Solana Onchain Actions",
-        description: "Crossmint Solana integration plugin",
-        providers: [getWalletProvider(walletClient)],
-        evaluators: [],
-        services: [],
-        actions: actions,
-    };
+  const { mint } = crossmint(getSetting("CROSSMINT_API_KEY"));
+
+  console.log("walletClient", walletClient);
+  const actions = await getOnChainActions({
+    wallet: walletClient,
+    // Add plugins here based on what actions you want to use
+    // See all available plugins at https://ohmygoat.dev/chains-wallets-plugins#plugins
+    plugins: [
+      // Add you solana plugins here
+      sendSOL(),
+      jupiter(),
+      splToken({
+        connection,
+        network: "mainnet"
+      }),
+      mint()
+      // coingecko({
+      //  apiKey: getSetting("COINGECKO_API_KEY")
+      // })
+    ]
+  });
+
+  return {
+    name: "[Crossmint] Solana Onchain Actions",
+    description: "Crossmint Solana integration plugin",
+    providers: [getWalletProvider(walletClient)],
+    evaluators: [],
+    services: [],
+    actions: actions
+  };
 }
 
 export default createCrossmintPlugin;
